b376db5609a56c5bce72df6ad346782419113fdc
J0Min <rudals489@naver.com>
2026-06-22 11:44:11 +0900

    Spanwer 시간 기반 난이도 데이터 활용 몬스터 스폰설정 SpawnData연동

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,9 +7,13 @@ public class Spawner : MonoBehaviour
 {
     //적 생성 위치들(스포너+스폰포인트들)
     public Transform[] spawnPoint;
+    //난이도별 스폰 설정 데이터
+    public SpawnData[] spawnData;
     //누적 시간 변수
     public float timer;
-
+    //현재 난이도(게임 시간에 따라 변동)
+    private int difficulty;
+    
     private void Awake()
     {
         //스포너 자신 + 자식 트랜스폼을 한번에 가져옴
@@ -20,8 +25,15 @@ public class Spawner : MonoBehaviour
     {
         //매 프레임마다 경과 시간 누적
         timer += Time.deltaTime;
+        //난이도 상승 연산
+        difficulty = (int)(GameManager.instance.gameTime / 10f);
+        //난이도가 데이터 개수를 넘지 않도록 제한(배열 범위 초과 방지)
+        if (difficulty >= spawnData.Length)
+        {
+            difficulty = spawnData.Length - 1;
+        }
         //0.2초마다 스폰호출
-        if (timer > 0.2f)
+        if (timer > spawnData[difficulty].spawnTime)
         {
             timer = 0;
             Spawn();
@@ -30,9 +42,11 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        //풀에서 적을 꺼냄
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, GameManager.instance.pool.prefabs.Length));
+        //현재 난이도 데이터가 지정한 종류(spriteType)의 프리팹을 풀에서 적을 꺼냄
+        GameObject enemy = GameManager.instance.pool.Get(spawnData[difficulty].spriteType);
         //스폰 포인트 중 하나에 배칭(랜덤, point들에서만 시작하기위해 0제외)
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        //현재 난이도 데이터로 Enemy 능력치 초기화(체력,속도)
+        enemy.GetComponent<Enemy>().Init();
     }
 }
