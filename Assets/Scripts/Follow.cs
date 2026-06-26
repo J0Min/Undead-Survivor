71708ef84b6be1b904fa934e81c5e7fdf6a19898
J0Min <rudals489@naver.com>
2026-06-26 11:49:19 +0900

    fix HUD의 무한 레벨업과 배열 넘침 예외 픽스

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -12,8 +12,9 @@ public class Follow : MonoBehaviour
 
     void Update()
     {
-        //UI의 Canvas 스크린 좌표계를 사용, 따라서 변환 필요
-        //Camera.main.WorldToScreenPoint
+        // UI의 Canvas 스크린 좌표계를 사용함, 따라서 변환이 필요
+        // Camera.main.WorldToScreenPoint이 월드좌표->스크린좌표로 변환
+        // 헬스바 체력UI가 플레이어 위치를 따라다니게 함
         rect.position = Camera.main.WorldToScreenPoint(GameManager.instance.player.transform.position);
     }
 }
