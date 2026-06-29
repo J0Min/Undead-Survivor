ab92d8412857c1a82861b2d2f04400413b7d1006
J0Min <rudals489@naver.com>
2026-06-29 11:36:33 +0900

    ItemData 데이터 에셋 구조 생성

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -8,6 +8,19 @@ public class ItemData : ScriptableObject
     //아이템 경로
     public enum ItemType{Melee, Range, Glove, Shoe, Heal}
 
-    public ItemType type; //선택된 아이템의 종류
+    public ItemType type;   //선택된 아이템의 종류
+    public int itemId;      //아이템 고유 번호
+    public string itemName; //아이템 이름(UI표시)
+    public string itemDesc; //아이템 설명(UI표시)
+    public Sprite itemIcon; //아이템 아이콘(UI표시)
+    
+    [Header("# Level Data")]
+    public float baseDamage;//0레벨 기본 데미지
+    public int baseCount;   //기본 갯수(근접은 칼날수,원거리는 관통수)
+    public float[] damages; //레벨별 데미지 증가량
+    public int[] counts;    //레벨별 갯수 증가량
+
+    [Header("# Weapon")] 
+    public GameObject projectile;   //무기가 사용할 투사체 프리팹
 
 }
